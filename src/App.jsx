3e455ce80ba917716a26c6bdf5fdf2a0dc79a32d
kyawphyothu <kyawphyothukpt256@gmail.com>
2023-08-25 01:04:20 +0630

    top page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Main from "./components/Main";
+import Top from "./components/Top";
+
 export default function App() {
 	return (
-	  <h1 className="text-3xl font-bold underline">
-		Hello world!
-	  </h1>
+		<div>
+			{/* top */}
+			<Top />
+			{/* <Main /> */}
+		</div>
 	)
   }
---
+++ src/components/Top.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import profile from "../assets/profile.jpg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode } from '@fortawesome/free-solid-svg-icons'
+
+export default function Top() {
+	return (
+		<div className="h-screen bg-gradient-to-tr from-gray-700 to-black text-white flex justify-center items-center text-center">
+			<div className='flex flex-col items-center'>
+				<img src={profile} className="w-40 h-40 rounded-full outline outline-offset-4 outline-teal-600 mb-10" />
+				<h1 className='font-semibold text-4xl mb-3'>
+					<span className='text-transparent bg-clip-text bg-gradient-to-tr from-yellow-300 to-red-500'>K</span>yaw Phyo Thu
+				</h1>
+				<p className='flex items-center mb-3'>
+					<FontAwesomeIcon icon={faCode} className='mr-3' />
+					<h1 className='text-2xl'>Web Developer</h1>
+				</p>
+				<p className=' text-neutral-400'>
+					Full-Stack Developer proficient in Laravel, React, and Express.
+				</p>
+			</div>
+		</div>
+	)
+}
